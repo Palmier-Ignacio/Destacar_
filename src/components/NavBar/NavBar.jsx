41b79d525293daffd13fdb03b7ba8310d564fb7b
Palmier-Ignacio <palmier.ignacio.nicolas@gmail.com>
2025-01-12 02:44:10 -0300

    responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,23 @@ function NavBar() {
             <div className="logo-container">
                 <Link to="/" className="logo"></Link>
             </div>
-            <nav>
+            <nav className="nav-web">
+                <ul>
+                    <li><Link to="/potencia-tus-redes">Potencia tus redes</Link></li>
+                    <li><div className="linea"></div></li>
+                    <li><Link to="/#testimonios">Testimonios</Link></li>
+                    <li><div className="linea"></div></li>
+                    <li><Link to="/#sobreMi">Sobre mi</Link></li>
+                    <li><div className="linea"></div></li>
+                    <li><Link to="/#contacto">Contacto</Link></li>
+                </ul>
+            </nav>
+            <button className="nav-movile-button">
+                <div className="nav-movile-button-line"></div>
+                <div className="nav-movile-button-line"></div>
+                <div className="nav-movile-button-line"></div>
+            </button>
+            <nav className="nav-movile">
                 <ul>
                     <li><Link to="/potencia-tus-redes">Potencia tus redes</Link></li>
                     <li><div className="linea"></div></li>
